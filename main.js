2c773470015d205397870b48d7ceb76d1ce363a0
vivek1030 <viveksinha1030@gmail.com>
2020-12-14 01:12:42 +0530

    fixed navigation on slide added

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+// Toggle menu 
 document.querySelector('.btn-dropdown').onclick = (e)=>{
+    e.preventDefault();
     document.querySelector('.dropdown-drawer').classList.toggle('active');
+}
+
+
+//navigation fixed scroll script
+window.onscroll = ()=>{
+    let val = window.pageYOffset;
+    console.log(val);
+    if(val > 80){
+        document.querySelector('.headermain').classList.remove('static');
+        document.querySelector('.headermain').classList.add('fixed');
+    }
+    else{
+        document.querySelector('.headermain').classList.add('static');
+        document.querySelector('.headermain').classList.remove('fixed');
+    }
 }
